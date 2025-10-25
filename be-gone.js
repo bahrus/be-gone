df9fa77ea0bbf962964f2771a5ad59ab1fa76956
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-10-25 07:40:24 -0400

    Update HTML example in README.md

--- be-gone.js
+++ be-gone.js
@@ -3,10 +3,41 @@ import { propInfo, rejected, resolved } from 'be-enhanced/cc.js';
 import { BE } from 'be-enhanced/BE.js';
 import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AllProps, AP} from './ts-refs/be-a-beacon/types' */;
+/** @import {Actions, PAP, AllProps, AP, BAP} from './ts-refs/be-gone/types' */;
 
 /**
  * @implements {Actions}
  * 
  */
-class 
+class BeGone extends BE {
+    /**
+     * @type {BEConfig<AP & BEAllProps, Actions & IEnhancement>}
+     */
+    static config = {
+        propInfo: {
+            ...propInfo,
+        },
+        positractions: [resolved, rejected],
+    };
+
+    de = de;
+
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
+    async onOnDefined(self){
+        const {onDefined, enhancedElement} = self;
+        for(const def of onDefined){
+            await customElements.whenDefined(def);
+        }
+        enhancedElement.remove();
+        return /** @type {PAP} */ ({
+            resolved: true
+        });
+    }
+}
+
+await BeGone.bootUp();
+export {BeGone}
